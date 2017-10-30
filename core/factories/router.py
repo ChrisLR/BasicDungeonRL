c561749ee9da29262de6a4114a6043b3963d1a20
nethys <arzhul@gmail.com>
2017-10-30 16:54:31 -0400

    Added to the router and spawn generator.

---
+++ core/factories/router.py
@@ -0,0 +1,22 @@
+from bflib.characters.base import Character
+from bflib.items.base import Item
+from bflib.monsters.base import Monster
+from core.factories.character import CharacterFactory
+from core.factories.items import ItemFactory
+from core.factories.monster import MonsterFactory
+
+type_mapping = {
+    Item: ItemFactory,
+    Character: CharacterFactory,
+    Monster: MonsterFactory,
+}
+types = [Item, Character, Monster]
+
+
+def route_to_factory(base_object_type):
+    for base_type in types:
+        if issubclass(base_object_type, base_type):
+            factory = type_mapping.get(base_type, None)
+            if factory is None:
+                raise Exception("No factory for base type {}".format(base_type))
+            return factory.create_new(base_type)
--- core/generators/spawns.py
+++ core/generators/spawns.py
@@ -1,17 +1,29 @@
+import random
+from core.factories.router import route_to_factory
+
+
 class OnceSpawner(object):
     """
     A Spawner that spawns only ONE set.
     """
+    __slots__ = ["spawn_sets"]
+
     def __init__(self, spawn_sets):
         self.spawn_sets = spawn_sets
 
     def spawn_objects(self):
-        pass
+        randomized_spawns = sorted(self.spawn_sets, key=random.randint(0, 100))
+        for spawn_set in randomized_spawns:
+            if random.randrange(1, 100) <= spawn_set.percent:
+                return [route_to_factory(spawn_type) for spawn_type in spawn_set.spawn_types]
 
 
 class SpawnSet(object):
     """
     A Set Containing a Percent and a Spawn Type.
     """
-    def __init__(self, percent, spawn_type):
-        pass
+    __slots__ = ["percent", "spawn_types"]
+
+    def __init__(self, percent, *spawn_types):
+        self.percent = percent
+        self.spawn_types = spawn_types
