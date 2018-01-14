3771a0f7668e10ca426b74e9c5c747eae66f4139
ChrisLR <arzhul@gmail.com>
2018-01-13 19:12:40 -0500

    Refactored connectors write, simpler, better.

--- core/maps/base/connector.py
+++ core/maps/base/connector.py
@@ -1,3 +1,6 @@
+from core import direction
+
+
 class Connector(object):
     """
     This Connector will link possible rooms together.
@@ -17,6 +20,15 @@ class Connector(object):
 
     def write(self, level, origin, direction, new_piece, new_origin):
         pass
+
+    def get_level_coordinates(self, origin):
+        ox, oy = origin
+        return [(ox + cx, oy + cy) for cx, cy in self.local_coordinates]
+
+    def get_opposite_level_coordinate(self, level_coordinate, step_direction):
+        ox, oy = direction.move_direction_mapping.get(step_direction)
+        lx, ly = level_coordinate
+        return lx + ox, ly + oy
 
     def get_adjacent_coordinates(self, target_coordinate):
         neighbors = []
--- core/maps/skeletoncrypt/connectors.py
+++ core/maps/skeletoncrypt/connectors.py
@@ -1,6 +1,5 @@
 from core.maps.base.connector import Connector
 from core.tiles import doors, floors
-from core.direction import move_direction_mapping, get_inverse_direction
 
 
 class DungeonDoorConnectorA(Connector):
@@ -8,65 +7,35 @@ class DungeonDoorConnectorA(Connector):
     # TODO A rusty/locked door, requiring bashing/picking
 
     def write(self, level, origin, direction, new_piece, new_origin):
-        inverse_direction = get_inverse_direction(direction)
-        origin_x, origin_y = origin
-        new_origin_x, new_origin_y = new_origin
-        compatible_connector = next(
-            (connector for connector
-             in new_piece.connectors.get(inverse_direction)
-             if isinstance(connector, type(self))))
-        other_neighbors = compatible_connector.local_coordinates
-        real_other_neighbors = [
-            (x + new_origin_x, y + new_origin_y)
-            for x, y in other_neighbors]
+        confirmed_coordinates = set()
+        confirmed_other_coordinates = set()
+        level_coordinates = self.get_level_coordinates(origin)
+        for lx, ly in level_coordinates:
+            opposite_coordinate = self.get_opposite_level_coordinate((lx, ly), direction)
+            if level.get_tile(opposite_coordinate):
+                confirmed_coordinates.add((lx, ly))
+                confirmed_other_coordinates.add(opposite_coordinate)
 
-        real_neighbors = [
-            (x + origin_x, y + origin_y)
-            for x, y in self.local_coordinates]
-        offset_x, offset_y = move_direction_mapping.get(direction)
-        confirmed_neighbors = set()
-        confirmed_other_neighbors = set()
-        for neighbor_x, neighbor_y in real_neighbors:
-            target_neighbor = (neighbor_x + offset_x, neighbor_y + offset_y)
-            if target_neighbor in real_other_neighbors:
-                confirmed_neighbors.add((neighbor_x, neighbor_y))
-                confirmed_other_neighbors.add(target_neighbor)
+        for level_coordinate in confirmed_coordinates:
+            level.add_tile(level_coordinate, doors.DungeonDoor())
 
-        for coordinate in confirmed_neighbors:
-            level.add_tile(coordinate, doors.DungeonDoor())
-
-        for coordinate in confirmed_other_neighbors:
-            level.add_tile(coordinate, floors.DungeonFloor())
+        for level_coordinate in confirmed_other_coordinates:
+            level.add_tile(level_coordinate, floors.DungeonFloor())
 
 
 class DungeonFloorConnectorA(Connector):
     def write(self, level, origin, direction, new_piece, new_origin):
-        inverse_direction = get_inverse_direction(direction)
-        origin_x, origin_y = origin
-        new_origin_x, new_origin_y = new_origin
-        compatible_connector = next(
-            (connector for connector
-             in new_piece.connectors.get(inverse_direction)
-             if isinstance(connector, type(self))))
-        other_neighbors = compatible_connector.local_coordinates
-        real_other_neighbors = [
-            (x + new_origin_x, y + new_origin_y)
-            for x, y in other_neighbors]
+        confirmed_coordinates = set()
+        confirmed_other_coordinates = set()
+        level_coordinates = self.get_level_coordinates(origin)
+        for lx, ly in level_coordinates:
+            opposite_coordinate = self.get_opposite_level_coordinate((lx, ly), direction)
+            if level.get_tile(opposite_coordinate):
+                confirmed_coordinates.add((lx, ly))
+                confirmed_other_coordinates.add(opposite_coordinate)
 
-        real_neighbors = [
-            (x + origin_x, y + origin_y)
-            for x, y in self.local_coordinates]
-        offset_x, offset_y = move_direction_mapping.get(direction)
-        confirmed_neighbors = set()
-        confirmed_other_neighbors = set()
-        for neighbor_x, neighbor_y in real_neighbors:
-            target_neighbor = (neighbor_x + offset_x, neighbor_y + offset_y)
-            if target_neighbor in real_other_neighbors:
-                confirmed_neighbors.add((neighbor_x, neighbor_y))
-                confirmed_other_neighbors.add(target_neighbor)
+        for level_coordinate in confirmed_coordinates:
+            level.add_tile(level_coordinate, floors.DungeonFloor())
 
-        for coordinate in confirmed_neighbors:
-            level.add_tile(coordinate, floors.DungeonFloor())
-
-        for coordinate in confirmed_other_neighbors:
-            level.add_tile(coordinate, floors.DungeonFloor())
+        for level_coordinate in confirmed_other_coordinates:
+            level.add_tile(level_coordinate, floors.DungeonFloor())
