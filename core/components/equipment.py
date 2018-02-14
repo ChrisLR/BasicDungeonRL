d69a9e7dbc57c3bd61e550c4186e5fa8a1b312ee
ChrisLR <arzhul@gmail.com>
2018-02-14 16:31:33 -0500

    Equipment will now properly unequip multi slot items.

--- core/components/equipment.py
+++ core/components/equipment.py
@@ -39,31 +39,31 @@ class Equipment(Component):
         return new_equipment
 
     def remove(self, item):
-        found_location = None
+        found_locations = []
         for location, worn_item in self.worn_items.items():
             if worn_item == item:
-                found_location = location
-                break
+                found_locations.append(location)
 
-        if found_location:
-            self.empty_wield_locations.append(found_location)
-            del self.worn_items[found_location]
+        if found_locations:
+            for location in found_locations:
+                self.empty_wield_locations.append(location)
+                del self.worn_items[location]
             return True
 
         for location, wielded_item in self.wielded_items.items():
             if wielded_item == item:
-                found_location = location
-                break
+                found_locations.append(location)
 
-        if found_location:
-            self.empty_wield_locations.append(found_location)
-            del self.wielded_items[found_location]
+        if found_locations:
+            for location in found_locations:
+                self.empty_wield_locations.append(location)
+                del self.wielded_items[location]
             return True
 
         return False
 
     def wear(self, item):
-        if self.armor_restrictions and not self.armor_restrictions.can_wear(item):
+        if self.armor_restrictions and not self.armor_restrictions.can_wear(item.base):
             return False
 
         if not item.wearable:
@@ -87,12 +87,12 @@ class Equipment(Component):
         return False
 
     def wield(self, item):
-        if self.weapon_restrictions and not self.weapon_restrictions.can_wield(item):
+        if self.weapon_restrictions and not self.weapon_restrictions.can_wield(item.base):
             return False
 
         hands = 1
         if self.weapon_size_restrictions:
-            keyword = self.weapon_size_restrictions.can_wield(item)
+            keyword = self.weapon_size_restrictions.can_wield(item.base)
             if not keyword:
                 return False
             else:
