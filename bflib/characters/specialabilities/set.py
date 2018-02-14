0af6e7a88b6629b41dd0d326338dd9fb85486efc
ChrisLR <arzhul@gmail.com>
2018-02-14 16:32:19 -0500

    Made the SpecialAbilitySet Iterable

--- bflib/characters/specialabilities/set.py
+++ bflib/characters/specialabilities/set.py
@@ -1,5 +1,16 @@
+from collections import Iterable
+
+
 class SpecialAbilitySet(object):
     __slots__ = ["special_abilities"]
 
     def __init__(self, special_abilities=None):
         self.special_abilities = special_abilities
+
+    def __iter__(self):
+        if isinstance(self.special_abilities, Iterable):
+            for special_ability in self.special_abilities:
+                yield special_ability
+        else:
+            yield self.special_abilities
+            raise StopIteration
