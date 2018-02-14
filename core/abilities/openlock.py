86ce42b13e0fb1dc7f26e0af8b7a04fabeeb4368
ChrisLR <arzhul@gmail.com>
2018-02-14 16:36:12 -0500

    Fixed the OpenLock Ability.

--- core/abilities/openlock.py
+++ core/abilities/openlock.py
@@ -1,13 +1,20 @@
 from bflib import dice
 from bflib.characters.specialabilities import OpenLock as OpenLockAbility
+from core import components
 from core.abilities.base import Ability
 from services import echo
 from services.selection import DirectionalSelection, filters, TargetSelectionSet
 
 
+class OnlyWithLocks(filters.Component):
+    component = components.Lock
+
+
 class OpenLock(Ability):
     name = "Open Lock"
-    target_selection = TargetSelectionSet(DirectionalSelection, filters.SingleListBased)
+    target_selection = TargetSelectionSet(
+        selections=DirectionalSelection,
+        filters=(OnlyWithLocks, filters.SingleListBased))
 
     @classmethod
     def can_execute(cls, character, target_selection=None):
@@ -19,25 +26,31 @@ class OpenLock(Ability):
                 echo.echo_service.echo("You can not make another attempt this level.")
             return False
 
-        if character.query.special_ability(OpenLockAbility).sum() <= 0:
+        if character.query.special_ability(OpenLockAbility) <= 0:
             return False
 
         return True
 
     @classmethod
     def execute(cls, character, target_selection=None):
-        open_lock_value = character.query.special_ability(OpenLockAbility).sum()
+        open_lock_value = character.query.special_ability(OpenLockAbility)
         item = target_selection[0]
         value = dice.D100(1).roll()
         if value > open_lock_value:
+            if echo.is_player(character):
+                echo.echo_service.echo(
+                    "You fail at picking {}'s lock!".format(item.name))
+            else:
+                echo.echo_service.echo(
+                    "{} fails at picking {}'s lock!".format(character.name, item.name))
             item.lock.add_failed_attempt(character, character.experience.level)
             return False
         else:
             item.lock.unlock()
             if echo.is_player(character):
                 echo.echo_service.echo(
-                    "You succeed in picking {}'s lock!.".format(item.name))
+                    "You succeed in picking {}'s lock!".format(item.name))
             else:
                 echo.echo_service.echo(
-                    "{} succeeds in picking {}'s lock!.".format(character.name, item.name))
+                    "{} succeeds in picking {}'s lock!".format(character.name, item.name))
         return True
