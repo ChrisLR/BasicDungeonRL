cb09762e7e69816419c2f6def33ec7ec7a190ffb
ChrisLR <arzhul@gmail.com>
2018-04-29 10:37:13 -0400

    Fixed actions not being reinstanciated when using action mapping

--- core/actionmapping.py
+++ core/actionmapping.py
@@ -5,26 +5,34 @@ class ActionMapping(object):
     def __init__(self, game):
         self.game = game
         self.lowercase = {
-            terminal.TK_KP_7: game.actions.get_action_by_name("walk_nw"),
-            terminal.TK_KP_8: game.actions.get_action_by_name("walk_n"),
-            terminal.TK_KP_9: game.actions.get_action_by_name("walk_ne"),
-            terminal.TK_KP_6: game.actions.get_action_by_name("walk_e"),
-            terminal.TK_KP_3: game.actions.get_action_by_name("walk_se"),
-            terminal.TK_KP_2: game.actions.get_action_by_name("walk_s"),
-            terminal.TK_KP_1: game.actions.get_action_by_name("walk_sw"),
-            terminal.TK_KP_4: game.actions.get_action_by_name("walk_w"),
-            terminal.TK_E: game.actions.get_action_by_name("eat"),
-            terminal.TK_O: game.actions.get_action_by_name("open"),
-            terminal.TK_C: game.actions.get_action_by_name("close"),
-            terminal.TK_G: game.actions.get_action_by_name("get"),
-            terminal.TK_W: game.actions.get_action_by_name("wear"),
-            terminal.TK_R: game.actions.get_action_by_name("remove"),
-            terminal.TK_D: game.actions.get_action_by_name("drop"),
-            terminal.TK_X: game.actions.get_action_by_name("look"),
-            terminal.TK_P: game.actions.get_action_by_name("put"),
-            terminal.TK_A: game.actions.get_action_by_name("use_ability"),
-            terminal.TK_F: game.actions.get_action_by_name("fire"),
+            terminal.TK_KP_7: "walk_nw",
+            terminal.TK_KP_8: "walk_n",
+            terminal.TK_KP_9: "walk_ne",
+            terminal.TK_KP_6: "walk_e",
+            terminal.TK_KP_3: "walk_se",
+            terminal.TK_KP_2: "walk_s",
+            terminal.TK_KP_1: "walk_sw",
+            terminal.TK_KP_4: "walk_w",
+            terminal.TK_E: "eat",
+            terminal.TK_O: "open",
+            terminal.TK_C: "close",
+            terminal.TK_G: "get",
+            terminal.TK_W: "wear",
+            terminal.TK_R: "remove",
+            terminal.TK_D: "drop",
+            terminal.TK_X: "look",
+            terminal.TK_P: "put",
+            terminal.TK_A: "use_ability",
+            terminal.TK_F: "fire",
         }
         self.uppercase = {
-            terminal.TK_W: game.actions.get_action_by_name("wield"),
+            terminal.TK_W: "wield",
         }
+
+    def get_lowercase(self, key):
+        name = self.lowercase.get(key)
+        return self.game.actions.get_action_by_name(name)
+
+    def get_uppercase(self, key):
+        name = self.uppercase.get(key)
+        return self.game.actions.get_action_by_name(name)
